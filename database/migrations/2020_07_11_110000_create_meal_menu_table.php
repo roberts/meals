cd51e3914a0a89b8b813ae8834a65b8fd3cfd29b
drewroberts <github@drewroberts.com>
2021-03-09 02:20:36 -0500

    Create pivot table for meal menu #6 #7

---
+++ database/migrations/2020_07_11_110000_create_meal_menu_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealMenuTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('meal_menu', function (Blueprint $table) {
+            $table->foreignId('meal_id')->references('id')->on('meals');
+            $table->foreignId('menu_id')->references('id')->on('menus');
+            $table->timestamps();
+        });
+    }
+}
