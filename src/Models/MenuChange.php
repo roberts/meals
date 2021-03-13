a4c5c837d4d29c991694da1ff2a51f755a2945c7
drewroberts <github@drewroberts.com>
2021-03-13 14:31:41 -0500

    Create model & factory for menu change #8

---
+++ src/Models/MenuChange.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roberts\Meals\Models;
+
+use Tipoff\Support\Models\BaseModel;
+use Tipoff\Support\Traits\HasPackageFactory;
+
+class MenuChange extends BaseModel
+{
+    use HasPackageFactory;
+}
