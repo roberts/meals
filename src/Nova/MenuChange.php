12f2fd8be8a22cfa7d1e0bd58f6db96994f08737
drewroberts <drewroberts@users.noreply.github.com>
2021-03-17 14:59:05 +0000

    Fix styling

--- src/Nova/MenuChange.php
+++ src/Nova/MenuChange.php
@@ -3,13 +3,7 @@
 namespace Roberts\Meals\Nova;
 
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\BelongsToMany;
-use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Number;
-use Laravel\Nova\Fields\Slug;
-use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
 use Tipoff\Support\Nova\BaseResource;
