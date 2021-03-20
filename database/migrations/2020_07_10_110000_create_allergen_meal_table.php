fd48690ba54008c790b72012a1cf3f02e22b8ed2
Drew Roberts <github@drewroberts.com>
2021-03-19 20:32:53 -0400

    Update migration for allergen meal pivot

--- database/migrations/2020_07_10_110000_create_allergen_meal_table.php
+++ database/migrations/2020_07_10_110000_create_allergen_meal_table.php
@@ -3,16 +3,16 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Roberts\Meals\Models\Allergen;
+use Roberts\Meals\Models\Meal;
 
 class CreateAllergenMealTable extends Migration
 {
     public function up()
     {
         Schema::create('allergen_meal', function (Blueprint $table) {
-            $table->foreignId('allergen_id')->references('id')->on('allergens');
-            $table->foreignId('meal_id')->references('id')->on('meals');
-            // $table->foreignIdFor(Allergen::class);
-            // $table->foreignIdFor(Meal::class);
+            $table->foreignIdFor(Allergen::class);
+            $table->foreignIdFor(Meal::class);
             $table->timestamps();
         });
     }
